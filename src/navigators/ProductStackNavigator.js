519cde3312d5ffc1129afdf38e70cec335f6fa47
Quentin Machard <quentin.machard@gmail.com>
2018-03-08 13:35:08 +0100

    Refactor ProductStackNavigator

---
+++ src/navigators/ProductStackNavigator.js
@@ -0,0 +1,22 @@
+import {StackNavigator} from "react-navigation";
+
+import style from "../assets/Style";
+import ProductsScreen from "../screens/ProductsScreen";
+import ProductDetailsScreen from "../screens/ProductDetailsScreen";
+
+const navigationOptions = {
+    headerStyle: style.header,
+    headerTitleStyle: style.headerTitle
+};
+
+
+export default StackNavigator({
+    ProductsScreen: {
+        screen: ProductsScreen,
+        navigationOptions
+    },
+    ProductDetailsScreen: {
+        screen: ProductDetailsScreen,
+        navigationOptions
+    }
+});
